cdc3d12ccded162c14a6ef63130561738ae1cc26
Jan T. Sott <git@idleberg.com>
2024-10-13 01:36:03 +0200

    update externals

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,22 +4,7 @@ export default defineConfig({
 	bundle: true,
 	clean: true,
 	entry: ['src/index.ts'],
-	external: [
-		// Atom
-		'atom',
-		'electron',
-
-		// Node
-		'assert',
-		'buffer',
-		'child_process',
-		'events',
-		'fs',
-		'os',
-		'path',
-		'stream',
-		'util',
-	],
+	external: ['atom', 'electron'],
 	format: 'cjs',
 	minify: true,
 	outDir: 'lib',
