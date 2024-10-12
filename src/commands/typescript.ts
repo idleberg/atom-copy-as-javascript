e2b105711fa004f6d9be6835e9b5e6afabbe2b02
Jan T. Sott <git@idleberg.com>
2024-10-12 12:53:36 +0200

    align error handling

--- src/commands/typescript.ts
+++ src/commands/typescript.ts
@@ -1,46 +1,41 @@
-import { getSelectedText, restoreWhitespace } from "../utils.ts";
-import * as prettier from "prettier";
-import * as ts from "typescript";
+import { getSelectedText, restoreWhitespace } from '../utils.ts';
+import * as prettier from 'prettier';
+import * as ts from 'typescript';
 import Config from '../config.ts';
-import normalizeNewlines from "normalize-newline";
+import normalizeNewlines from 'normalize-newline';
 
 const tsConfig: ts.CompilerOptions = {
-  allowJs: true,
-  jsx: ts.JsxEmit.Preserve,
-  removeComments: false,
-  target: ts.ScriptTarget.ESNext,
+	allowJs: true,
+	jsx: ts.JsxEmit.Preserve,
+	removeComments: false,
+	target: ts.ScriptTarget.ESNext,
 };
 
 export async function copyAsJavascript() {
-  const selectedText = getSelectedText();
+	const selectedText = getSelectedText();
 
-  if (!selectedText?.trim()) {
-    atom.notifications.addWarning("No Code Selected");
-    return;
-  }
+	if (!selectedText?.trim()) {
+		atom.notifications.addWarning('No Code Selected');
+		return;
+	}
 
-  try {
-    // need to normalize newlines to make sure diffing works correctly with carriage returns
-    const textWithNewLines = normalizeNewlines(selectedText);
+	try {
+		// need to normalize newlines to make sure diffing works correctly with carriage returns
+		const textWithNewLines = normalizeNewlines(selectedText);
 
-    const strippedSource = await prettier.format(
-      ts.transpile(textWithNewLines, tsConfig),
-      Config.get("prettierConfig")
-    );
+		const strippedSource = await prettier.format(
+			ts.transpile(textWithNewLines, tsConfig),
+			Config.get('prettierConfig'),
+		);
 
-    console.log(Config.get("prettierConfig"))
+		// reconstruct newlines to match the original source
+		const finalSource = restoreWhitespace(textWithNewLines, strippedSource).trim();
 
-    // reconstruct newlines to match the original source
-    const finalSource = restoreWhitespace(
-      textWithNewLines,
-      strippedSource
-    ).trim();
+		atom.clipboard.write(finalSource);
 
-    atom.clipboard.write(finalSource);
-
-    atom.notifications.addInfo("Copied as JavaScript");
-  } catch (err) {
-    console.log(err);
-    atom.notifications.addError("Failed to copy as JavaScript");
-  }
+		atom.notifications.addInfo('Copied as JavaScript');
+	} catch (error) {
+		console.error(error);
+		atom.notifications.addError('Failed to copy as JavaScript');
+	}
 }
--- src/config.ts
+++ src/config.ts
@@ -2,142 +2,138 @@ import type { ConfigValues } from 'atom';
 import { name } from '../package.json';
 
 export default {
-  schema: {
-    prettierConfig: {
-      title: 'Prettier Configuratiion',
-      description: '',
-      type: 'object',
-      order: 0,
-      properties: {
-        parser: {
-          title: 'TypeScript Parser',
-          description: 'Specify which parser to use. See [documentation](https://prettier.io/docs/en/options#parser) for details.',
-          type: 'string',
-          enum: [
-            'babel-ts',
-            'typescript',
-          ],
-          default: 'babel-ts',
-          order: 0,
-        },
-        printWidth: {
-          title: 'Print Width',
-          description: 'Specify the line length that the printer will wrap on. See [documentation](https://prettier.io/docs/en/options#print-width) for details.',
-          type: 'number',
-          default: 80,
-          minimum: 80,
-          order: 1,
-        },
-        useTabs: {
-          title: 'Use Tabs',
-          description: 'Indent lines with tabs instead of spaces. See [documentation](https://prettier.io/docs/en/options#tab-width) for details.',
-          type: 'boolean',
-          default: true,
-          order: 2,
-        },
-        tabWidth: {
-          title: 'Tab Width',
-          description: 'Specify the number of spaces per indentation-level. See [documentation](https://prettier.io/docs/en/options#tab-width) for details.',
-          type: 'number',
-          default: 2,
-          minimum: 0,
-          maximum: 8,
-          order: 3,
-        },
-        singleQuote: {
-          title: 'Single Quote',
-          description: 'Use single quotes instead of double quotes. See [documentation](https://prettier.io/docs/en/options#quotes) for details.',
-          type: 'boolean',
-          default: true,
-          order: 4,
-        },
-        noSemi: {
-          title: 'No Semi',
-          description: 'Use single quotes instead of double quotes. See [documentation](https://prettier.io/docs/en/options#quotes) for details.',
-          type: 'boolean',
-          default: false,
-          order: 5,
-        },
-        quoteProps: {
-          title: 'Quote Props',
-          description: 'Change when properties in objects are quoted. See [documentation](https://prettier.io/docs/en/options#quote-props) for details.',
-          type: 'string',
-          enum: [
-            'as-needed',
-            'consistent',
-            'preserve',
-          ],
-          default: 'as-needed',
-          order: 6,
-        },
-        arrowParens: {
-          title: 'Arrow Function Parantheses',
-          description: 'Include parentheses around a sole arrow function parameter. See[documentation](https://prettier.io/docs/en/options#arrow-function-parentheses) for details.',
-          type: 'string',
-          enum: [
-            'always',
-            'avoid',
-          ],
-          default: 'always',
-          order: 7,
-        },
-        trailingComma: {
-          title: 'Trailing Comma',
-          description: 'Print trailing commas wherever possible in multi-line comma-separated syntactic structures. See[documentation](https://prettier.io/docs/en/options#trailing-commas) for details.',
-          type: 'string',
-          enum: [
-            'all',
-            'es5',
-            'none',
-          ],
-          default: 'all',
-          order: 8,
-        },
-        bracketSpacing: {
-          title: 'Trailing Comma',
-          description: 'Print spaces between brackets in object literals. See[documentation](https://prettier.io/docs/en/options#bracket-spacing) for details.',
-          type: 'boolean',
-          default: false,
-          order: 9,
-        },
-      },
-    },
-  },
+	schema: {
+		prettierConfig: {
+			title: 'Prettier Configuratiion',
+			description: '',
+			type: 'object',
+			order: 0,
+			properties: {
+				parser: {
+					title: 'TypeScript Parser',
+					description:
+						'Specify which parser to use. See [documentation](https://prettier.io/docs/en/options#parser) for details.',
+					type: 'string',
+					enum: ['babel-ts', 'typescript'],
+					default: 'babel-ts',
+					order: 0,
+				},
+				printWidth: {
+					title: 'Print Width',
+					description:
+						'Specify the line length that the printer will wrap on. See [documentation](https://prettier.io/docs/en/options#print-width) for details.',
+					type: 'number',
+					default: 80,
+					minimum: 80,
+					order: 1,
+				},
+				useTabs: {
+					title: 'Use Tabs',
+					description:
+						'Indent lines with tabs instead of spaces. See [documentation](https://prettier.io/docs/en/options#tab-width) for details.',
+					type: 'boolean',
+					default: true,
+					order: 2,
+				},
+				tabWidth: {
+					title: 'Tab Width',
+					description:
+						'Specify the number of spaces per indentation-level. See [documentation](https://prettier.io/docs/en/options#tab-width) for details.',
+					type: 'number',
+					default: 2,
+					minimum: 0,
+					maximum: 8,
+					order: 3,
+				},
+				singleQuote: {
+					title: 'Single Quote',
+					description:
+						'Use single quotes instead of double quotes. See [documentation](https://prettier.io/docs/en/options#quotes) for details.',
+					type: 'boolean',
+					default: true,
+					order: 4,
+				},
+				noSemi: {
+					title: 'No Semi',
+					description:
+						'Use single quotes instead of double quotes. See [documentation](https://prettier.io/docs/en/options#quotes) for details.',
+					type: 'boolean',
+					default: false,
+					order: 5,
+				},
+				quoteProps: {
+					title: 'Quote Props',
+					description:
+						'Change when properties in objects are quoted. See [documentation](https://prettier.io/docs/en/options#quote-props) for details.',
+					type: 'string',
+					enum: ['as-needed', 'consistent', 'preserve'],
+					default: 'as-needed',
+					order: 6,
+				},
+				arrowParens: {
+					title: 'Arrow Function Parantheses',
+					description:
+						'Include parentheses around a sole arrow function parameter. See[documentation](https://prettier.io/docs/en/options#arrow-function-parentheses) for details.',
+					type: 'string',
+					enum: ['always', 'avoid'],
+					default: 'always',
+					order: 7,
+				},
+				trailingComma: {
+					title: 'Trailing Comma',
+					description:
+						'Print trailing commas wherever possible in multi-line comma-separated syntactic structures. See[documentation](https://prettier.io/docs/en/options#trailing-commas) for details.',
+					type: 'string',
+					enum: ['all', 'es5', 'none'],
+					default: 'all',
+					order: 8,
+				},
+				bracketSpacing: {
+					title: 'Trailing Comma',
+					description:
+						'Print spaces between brackets in object literals. See[documentation](https://prettier.io/docs/en/options#bracket-spacing) for details.',
+					type: 'boolean',
+					default: false,
+					order: 9,
+				},
+			},
+		},
+	},
 
-  get(key = ''): ConfigValues {
-    return key?.length ? atom.config.get(`${name}.${key}`) : atom.config.get(`${name}`);
-  },
+	get(key = ''): ConfigValues {
+		return key?.length ? atom.config.get(`${name}.${key}`) : atom.config.get(`${name}`);
+	},
 
-  migrate(oldKey: string, newKey: string): void {
-    if (!atom.config.get(`${name}.${oldKey}`) || atom.config.get(`${name}.${newKey}`)) {
-      return;
-    }
+	migrate(oldKey: string, newKey: string): void {
+		if (!atom.config.get(`${name}.${oldKey}`) || atom.config.get(`${name}.${newKey}`)) {
+			return;
+		}
 
-    try {
-      atom.config.set(`${name}.${newKey}`, atom.config.get(`${name}.${oldKey}`));
-      /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
-    } catch (error) {
-      atom.notifications.addWarning(`Failed to migrate configuration, see console for details`);
+		try {
+			atom.config.set(`${name}.${newKey}`, atom.config.get(`${name}.${oldKey}`));
+		} catch (error) {
+			console.error(error);
+			atom.notifications.addWarning(`Failed to migrate configuration, see console for details`);
 
-      return;
-    }
+			return;
+		}
 
-    atom.config.unset(`${name}.${oldKey}`);
-  },
+		atom.config.unset(`${name}.${oldKey}`);
+	},
 
-  unset(key = ''): void {
-    const unsetKey = key?.length ? `${name}.${key}` : name;
+	unset(key = ''): void {
+		const unsetKey = key?.length ? `${name}.${key}` : name;
 
-    atom.config.unset(unsetKey);
-  },
+		atom.config.unset(unsetKey);
+	},
 
-  async open(options = {}): Promise<void> {
-    options = {
-      pending: true,
-      searchAllPanes: true,
-      ...options,
-    };
+	async open(options = {}): Promise<void> {
+		options = {
+			pending: true,
+			searchAllPanes: true,
+			...options,
+		};
 
-    await atom.workspace.open(`atom://config/packages/${name}`, options);
-  }
+		await atom.workspace.open(`atom://config/packages/${name}`, options);
+	},
 };
